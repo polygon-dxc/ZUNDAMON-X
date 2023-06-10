edfb20a1e2348f37ba07d32cb86339f04b3e0309
Minimalist-00 <kouki4122@icloud.com>
2023-06-10 19:52:04 +0900

    現在の

--- src/background/getVideoCurrentTime.tsx
+++ src/background/getVideoCurrentTime.tsx
@@ -1,31 +1,37 @@
-/* 現在の再生時刻を取得 */
-const getVideoCurrentTime = () => {
-  const onRun = () => {
-    const video = document.getElementsByTagName('video')[0];
-    console.log(video);
-    return video?.currentTime;
-  };
-
-  /* アクティブなタブを取得し、そのタブに対してスクリプトを実行する */
-  const scripting = async () => {
-    const [tab] = await chrome.tabs.query({
-      //アクティブなタブを取得
-      active: true,
-      currentWindow: true,
+/* アクティブなタブを取得し、そのタブに対してスクリプトを実行する */
+const scripting = async (onRun: () => number | boolean) => {
+  const [tab] = await chrome.tabs.query({
+    //アクティブなタブを取得
+    active: true,
+    currentWindow: true,
+  });
+  /* 取得したタブに対してスクリプトを実行 */
+  chrome.scripting
+    .executeScript({
+      target: { tabId: tab.id || 0 },
+      func: onRun,
+    })
+    .then((result) => {
+      // promiseで成功した時の処理
+      return result[0].result;
     });
-    /* 取得したタブに対してスクリプトを実行 */
-    chrome.scripting
-      .executeScript({
-        target: { tabId: tab.id || 0 },
-        func: onRun,
-      })
-      .then((result) => {
-        // promiseで成功した時の処理
-        return result[0].result;
-      });
-  };
-
-  scripting();
 };
 
-export default getVideoCurrentTime;
+/* 現在の再生時刻を取得 */
+export const getVideoCurrentTime = () => {
+  const videoCurrentTime = () => {
+    const video = document.getElementsByTagName('video')[0];
+    return video?.currentTime;
+  };
+  return scripting(videoCurrentTime);
+};
+
+export const getPlaybackStatus = () => {
+  const playbackStatus = () => {
+    const video = document.getElementsByTagName('video')[0];
+    return video?.paused;
+  };
+  return scripting(playbackStatus);
+};
+
+console.log(getPlaybackStatus());
