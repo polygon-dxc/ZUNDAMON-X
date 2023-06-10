75c87899d55c96ce83ac00c3913d6cde05a655a4
Minimalist-00 <kouki4122@icloud.com>
2023-06-10 15:30:23 +0900

    ポップアップにオプションページへの遷移を追加

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -3,9 +3,17 @@ import React, { ReactElement } from 'react';
 const Popup = (): ReactElement => {
   document.body.style.width = '15rem';
   document.body.style.height = '15rem';
+
+  const openOptionsPage = () => {
+    chrome.runtime.openOptionsPage(); //Chrome拡張のオプションページに遷移
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
       <h1>Popup</h1>
+      <div>
+        <button onClick={openOptionsPage}>オプションページを開く</button>
+      </div>
     </div>
   );
 };
