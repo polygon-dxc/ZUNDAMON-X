499c485b1c91a0637322dbc1ab609d4c2970d3ec
suis-cats <takkunkimidori@gmail.com>
2023-06-12 06:01:59 +0900

    GCPの字幕取得URLをコメントアウトで追加

--- src/background/getTranscript.ts
+++ src/background/getTranscript.ts
@@ -6,16 +6,27 @@ import { videoidtype } from '../types';
 async function getTranscript({ videoId }: videoidtype) {
   //字幕APIアクセスURL
   //http://127.0.0.1:8000/transcript/?id=wdvclbIHfHk
-  const response = await axios.get(`http://127.0.0.1:8000/transcript/`, {
+
+  /*
+  const response = await axios.get(`http://35.189.143.254/transcript`, {
     params: {
-      id: videoId,
+      videoId: videoId,
     },
   });
+  */
+  try {
+    const response = await axios.get(`http://127.0.0.1:8000/transcript/`, {
+      params: {
+        id: videoId,
+      },
+    });
 
-  // The data property of the response will contain the transcript
-  const transcript = response.data;
-
-  return transcript;
+    // The data property of the response will contain the transcript
+    const transcript = response.data;
+    return transcript;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default getTranscript;
