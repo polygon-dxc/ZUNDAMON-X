53de3bd9cd653e3e77ae91f43d94cd94d54f9375
Minimalist-00 <kouki4122@icloud.com>
2023-06-12 02:58:35 +0900

    [音声, startTime]のオブジェクトを作成する関数を作成
    字幕とstartを引数として、APIを叩いて音声ファイルとstartTimeを格納するオブジェクトを作成する関数をつくった

--- src/background/getVideoCurrentTime.tsx
+++ src/background/getVideoCurrentTime.tsx
@@ -18,6 +18,7 @@ const scripting = async (onRun: () => number | boolean) => {
 };
 
 /* 現在の再生時刻を取得 */
+// 例: 1分30秒の場合、90を返す
 export const getVideoCurrentTime = () => {
   const videoCurrentTime = () => {
     const video = document.getElementsByTagName('video')[0];
@@ -26,6 +27,9 @@ export const getVideoCurrentTime = () => {
   return scripting(videoCurrentTime);
 };
 
+/* 現在の再生状態を取得 */
+// true: 再生中
+// false: 停止中
 export const getPlaybackStatus = () => {
   const playbackStatus = () => {
     const video = document.getElementsByTagName('video')[0];
@@ -33,5 +37,3 @@ export const getPlaybackStatus = () => {
   };
   return scripting(playbackStatus);
 };
-
-console.log(getPlaybackStatus());
---
+++ src/content/useAudioData.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+const useAudioData = (subtitle: string, start: number) => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch('http://35.189.143.254/voice?message=' + subtitle)
+      .then((res: Response) => res.arrayBuffer())
+      .then((buffer: ArrayBuffer) => {
+        /* 音声データの変換処理 */
+        /* blobを使用する場合 */
+        const blob = new Blob([buffer], { type: 'audio/wav' }); // Blobはバイナリデータを扱うためのオブジェクト
+        const url = URL.createObjectURL(blob); // blobから音声のURLを作成
+        const audio = new Audio(); // Audioオブジェクトを作成
+        audio.src = url; // URLを指定
+
+        /* bufferを使用する場合 */
+        const wavFile = new File([buffer], 'filename.wav', { type: 'audio/wav' }); // bufferからwavファイルを作成
+        const wavUrl = URL.createObjectURL(wavFile); // wavファイルからURLを作成
+
+        /* 音声データの保存処理 */
+        // URLからファイル名を取得
+        const fileName = url.split('/').pop();
+        // ファイル名から拡張子を取得
+        const ext = fileName?.split('.').pop();
+        // ファイル名から拡張子を除いた名前を取得
+        const name = fileName?.replace('.' + ext, '');
+        // ファイル名を指定
+        const fileNameWithExt = name + '.wav';
+
+        setData((prevData) => ({
+          ...prevData,
+          [subtitle]: { voice: blob, startTime: start },
+        }));
+      });
+  }, [subtitle, start]);
+};
+
+export default useAudioData;
