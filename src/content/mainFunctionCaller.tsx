5d924175728472dcc53c951bc4c9167374c8d375
suis-cats <takkunkimidori@gmail.com>
2023-06-16 18:39:30 +0900

    デザイン修正

--- src/content/mainFunctionCaller.tsx
+++ src/content/mainFunctionCaller.tsx
@@ -218,55 +218,6 @@ const MainFunctionCaller = () => {
             </div>
           </div>
 
-          <div id="accordion-collapse" data-accordion="collapse">
-            <h2 id="accordion-collapse-heading-1">
-              <button
-                type="button"
-                className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-                data-accordion-target="#accordion-collapse-body-1"
-                aria-expanded="true"
-                aria-controls="accordion-collapse-body-1"
-              >
-                <span>What is Flowbite?</span>
-                <svg
-                  data-accordion-icon
-                  className="w-6 h-6 rotate-180 shrink-0"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
-            </h2>
-            <div
-              id="accordion-collapse-body-1"
-              className="hidden"
-              aria-labelledby="accordion-collapse-heading-1"
-            >
-              <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                <p className="mb-2 text-gray-500 dark:text-gray-400">
-                  Flowbite is an open-source library of interactive components built on top of
-                  Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
-                </p>
-                <p className="text-gray-500 dark:text-gray-400">
-                  Check out this guide to learn how to{' '}
-                  <a
-                    href="/docs/getting-started/introduction/"
-                    className="text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    get started
-                  </a>{' '}
-                  and start developing websites even faster with components on top of Tailwind CSS.
-                </p>
-              </div>
-            </div>
-          </div>
-
           <div className="log">
             <p>VideoID：{video.videoId ? video.videoId : ''}</p>
             <p>現在時刻-20：{(currentTime - 20).toFixed(3)}</p>
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -7,16 +7,10 @@ import AudioAnalyzerTest from './AudioAnalyzerTest';
 
 const Popup = (): ReactElement => {
   document.body.style.width = '18rem';
-  document.body.style.height = '10rem';
-
-  const openOptionsPage = () => {
-    chrome.runtime.openOptionsPage(); //Chrome拡張のオプションページに遷移
-  };
+  document.body.style.height = '15rem';
 
   return (
     <div className="h-screen items-center justify-center">
-      <h1>Popup</h1>
-      <button onClick={openOptionsPage}>オプションページを開く</button>
       <MainFunctionCaller />
     </div>
   );
