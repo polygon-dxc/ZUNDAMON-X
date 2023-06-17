997bc5876d451edf53b4be3c5d83ffdb585e53f1
aoi0412 <aoiaoi0412.1203@gmail.com>
2023-06-17 12:20:43 +0900

    marge yura project ver2

--- src/popup/AudioAnalyzer.tsx
+++ src/popup/AudioAnalyzer.tsx
@@ -1,132 +1,79 @@
 import React, { FC, useEffect, useState } from 'react';
 
-import image1 from './../../public/images/zundamon_content0000.png';
-import image2 from './../../public/images/zundamon_content0001.png';
-import { useRecoilValue } from 'recoil';
-import { audioDataState } from '../atom';
+import image1 from './../../public/images/zundamon_0001.png';
+import image2 from './../../public/images/zundamon_0002.png';
 
-type Props = {};
-const voiceThreshold = 100;
-const AudioAnalyzer: FC<Props> = () => {
+type Props = {
+  file: File | null;
+};
+const AudioAnalyzer: FC<Props> = ({ file }) => {
   const [number, setNumber] = useState(image1);
-  const [tmp, setTmp] = useState<Uint8Array>();
-  const audioData = useRecoilValue(audioDataState);
-  const { src, frequencyData } = audioData || {};
-
   useEffect(() => {
-    if (!src) return;
+    if (!file) return;
 
-    playElement();
-  }, [src]);
+    playFile();
+  }, [file]);
 
-  const playElement = () => {
-    if (!src) return;
-    const audio = new Audio();
+  const playFile = () => {
+    if (!file) return;
 
-    audio.src = src;
-    if (audio.onended)
-      audio.addEventListener('ended', () => {
-        setNumber(image1);
-      });
-    if (audio.onplay)
-      audio.addEventListener('play', () => {
-        setNumber(image2);
-      });
+    const audioContext = new (window.AudioContext || window.AudioContext)();
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      if (e.target) {
+        const arrayBuffer = e.target.result as ArrayBuffer;
+
+        audioContext.decodeAudioData(arrayBuffer, (buffer) => {
+          const source = audioContext.createBufferSource();
+          source.buffer = buffer;
+
+          const analyser = audioContext.createAnalyser();
+          analyser.fftSize = 2048;
+
+          source.connect(analyser);
+          analyser.connect(audioContext.destination);
+          source.onended = () => {
+            setNumber(image1);
+            source.stop();
+            source.disconnect();
+            clearInterval(interval);
+            audioContext.close();
+          };
+          const bufferLength = analyser.frequencyBinCount;
+          const dataArray = new Uint8Array(bufferLength);
+
+          source.start();
+          const interval = setInterval(() => {
+            analyser.getByteTimeDomainData(dataArray);
+            let sum = 0;
+            for (let i = 0; i < bufferLength; i++) {
+              const value = (dataArray[i] - 128) / 128;
+              sum += value * value;
+            }
+            const rms = Math.sqrt(sum / bufferLength);
+            if (rms >= 0.001) {
+              setNumber(image2);
+            } else {
+              setNumber(image1);
+            }
+          }, 500);
+        });
+      }
+    };
+
+    reader.readAsArrayBuffer(file);
   };
-  function getAverageVolume(array: Uint8Array) {
-    var values = 0;
-    var length = array.length;
-
-    // バッファ内のすべての値を合計する
-    for (var i = 0; i < length; i++) {
-      values += array[i];
-    }
-
-    // 平均値を計算する
-    var average = values / length;
-    return average;
-  }
 
   return (
     <div>
-      {/* <button onClick={playFile}>play</button> */}
-      <div style={{ width: '200px', height: '400px', zIndex: 9999 }}>
-        <div
-          style={{
-            position: 'relative',
-            zIndex: 100,
-            width: '120px',
-            height: '180px',
-            display: 'flex',
-            top: '100px',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <div style={{ position: 'absolute', zIndex: 0, top: '0px' }}>
-            <div>
-              <div
-                style={{
-                  width: '104px',
-                  height: '108px',
-                  borderRadius: '200px',
-                  border: '8px solid black',
-                  boxShadow: '0px 10px 10px -6px rgba(5, 4, 4, 0.3)',
-                  top: 0,
-                  backgroundColor: '#E1ECC8',
-                }}
-              ></div>
-            </div>
-          </div>
-
-          <div style={{ zIndex: 2, top: '64px', position: 'absolute' }}>
-            <div style={{ width: '120px', height: '60px', position: 'relative', zIndex: 2 }}>
-              <div
-                style={{
-                  width: '104px',
-                  height: '160px',
-                  position: 'absolute',
-                  borderBottomLeftRadius: '200px',
-                  borderBottomRightRadius: '200px',
-                  border: '8px solid rgba(0, 0, 0, 0)',
-                  bottom: 0,
-                  display: 'flex',
-                  justifyContent: 'center',
-                  overflow: 'hidden',
-                }}
-              >
-                {frequencyData ? (
-                  <img
-                    src={chrome.runtime.getURL(
-                      getAverageVolume(frequencyData) > 0 ? image2 : image1
-                    )}
-                    style={{
-                      borderBottomLeftRadius: '100px',
-                      borderBottomRightRadius: '100px',
-                      height: '300px',
-                      position: 'absolute',
-                      top: 0,
-                      zIndex: 1,
-                    }}
-                  />
-                ) : (
-                  <img
-                    src={chrome.runtime.getURL(image1)}
-                    style={{
-                      borderBottomLeftRadius: '100px',
-                      borderBottomRightRadius: '100px',
-                      height: '300px',
-                      position: 'absolute',
-                      top: 0,
-                      zIndex: 1,
-                    }}
-                  />
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <button onClick={playFile}>play</button>
+      <img
+        src={chrome.runtime.getURL(number)}
+        style={{
+          height: '100px',
+        }}
+      />
     </div>
   );
 };
---
+++ src/popup/background/useAudioData.ts
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { audioDataObject } from '../../types';
+
+const useAudioData = () => {
+  const [audioData, setAudioData] = useState<audioDataObject>({});
+
+  const getAudio = (subtitle: string, start: number) => {
+    fetch(
+      'https://asia-northeast1-zundamon-x.cloudfunctions.net/zundamon-api-proxy/voice?message=' +
+        subtitle
+    )
+      .then((res: Response) => {
+        return res.arrayBuffer();
+      })
+      .then((buffer: ArrayBuffer) => {
+        /* 音声データの変換処理 */
+        const wavFile = new File([buffer], 'filename.wav', { type: 'audio/wav' }); // bufferからwavファイルを作成
+
+        setAudioData((prevData) => ({
+          ...prevData,
+          [`${start}`]: wavFile, // data[startの時間] = wavファイル
+        }));
+      });
+  };
+
+  // console.log(audioData);
+
+  return { audioData, getAudio };
+};
+
+export default useAudioData;
