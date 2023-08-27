498f7553e46143abe16c130fddf7c18a3415b41b
suis-cats <takkunkimidori@gmail.com>
2023-08-27 19:19:55 +0900

    ダークモード時に黒色になるのを削除

--- src/options/CharactersSelector.tsx
+++ src/options/CharactersSelector.tsx
@@ -94,8 +94,8 @@ const CharacterSelector = () => {
                 className={`py-2.5 px-5 mr-2 mb-2 text-sm font-medium focus:outline-none rounded-full focus:ring-4 text-center 
                   ${
                     selectedStyle === style
-                      ? 'text-white bg-green-700 hover:bg-green-800 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
-                      : 'text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
+                      ? 'text-white bg-green-700 hover:bg-green-800 focus:ring-green-300'
+                      : 'text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-gray-200 '
                   }`}
               >
                 {style}
